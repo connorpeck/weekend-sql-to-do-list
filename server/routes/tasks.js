f4bef94d2ffab416a0c8d459cd066d416b21f804
connorpeck <90285369+connorpeck@users.noreply.github.com>
2022-05-19 17:02:11 -0500

    packages installed, spun up server

---
+++ server/routes/tasks.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const tasks = express.Router();
+const pool = require('../modules/pool');
+
+
+
+
+
+module.exports = tasks
---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 5001;
+const tasksRouter = require('./routes/tasks');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('server/public'));
+app.use('/tasks', tasksRouter);
+
+
+app.listen(port, ()=>{
+    console.log('listening on port:', port);
+});
