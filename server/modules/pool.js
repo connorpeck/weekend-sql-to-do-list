db04a7de7ed558f2887d7b3da624009670f84058
connorpeck <90285369+connorpeck@users.noreply.github.com>
2022-09-10 12:09:51 -0500

    Update pool.js

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,7 +1,7 @@
 const pg = require ('pg');
 const pool = new pg.Pool({
-    database: 'weekend-to-do-app',
-    host: 'localhost',
+    database: 'd4e1120bv8ikno',
+    host: 'ec2-44-205-64-253.compute-1.amazonaws.com',
     post: 5432,
     max: 12,
     idleTimeoutMillis: 30000
